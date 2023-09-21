f219238813804c95ef0f461e36c903201d9e5499
Sosna123 <olek.arez@gmail.com>
2023-09-21 20:52:06 +0200

    Navbar (kinda) complete

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-  </nav>
+  <Navbar />
   <router-view/>
 </template>
 
 <script>
+import Navbar from './views/Navbar.vue'
+export default {
+    components: {
+        Navbar
+    },
+    setup() {
 
+    }
+}
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&family=Montserrat:wght@300;400&display=swap');
 
+body{
+    margin: 0;
+    padding: 0;
+}
+
+p, a, h1, button{
+    font-family: "inter", sans-serif;
+}
 </style>
---
+++ src/views/Navbar.vue
@@ -0,0 +1,60 @@
+<template>
+    <nav class="nb">
+        <a class="nb-text" href="/">Olek Arez</a>
+        <a class="nb-link nb-link-first" href="/sites">My Sites</a>
+        <a class="nb-link" href="/sites">My Sites</a>
+        <a class="nb-link" href="/sites">My Sites</a>
+    </nav>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.nb{
+    display: flex;
+    background: rgb(0, 90, 255);
+    padding: 20px;
+    margin: 0;
+}
+
+    /* nb-text */
+
+.nb-text{
+    text-decoration: none;
+    color: white;
+    font-size: 46px;
+    margin: 0;
+}
+
+.nb-text:hover{
+    color: rgb(200, 200, 200);
+}
+
+.nb-text:active{
+    color: rgb(150, 150, 150);
+}
+
+    /* nb-link */
+
+.nb-link{
+    text-decoration: none;
+    color: white;
+    font-size: 32px;
+    margin: 0 5px;
+    align-self: center;
+}
+
+.nb-link-first{
+    margin-left: auto;
+}
+
+.nb-link:hover{
+    color: rgb(200, 200, 200);
+}
+
+.nb-link:active{
+    color: rgb(150, 150, 150);
+}
+</style>

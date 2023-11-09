3909e28523d8ee936aef9d3cf4365b74b3604923
Sosna123 <olek.arez@gmail.com>
2023-11-09 19:33:23 +0100

    Added some text (it isn't empty now)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
-    
+    <div class="container-fluid p-2 ms-4 justify-content-center">
+        <p class="display-6">Hello, I'm Olek</p>
+        <p class="lead fs-4">and this is my page.</p>
+    </div>
 </template>
 
 <script>
